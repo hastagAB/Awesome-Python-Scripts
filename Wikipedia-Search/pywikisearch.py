e0997d68e6933736866060ea923c1232e0857ac0
Nissaar <34180849+Nissaar@users.noreply.github.com>
2019-10-07 20:07:08 +0400

    Update pywikisearch.py

--- Wikipedia-Search/pywikisearch.py
+++ Wikipedia-Search/pywikisearch.py
@@ -4,4 +4,5 @@ print("Welcome to the app which find an answer to your question from wikipedia")
 
 while True:
     ques = input("What would you like to ask Wikipedia?")
-    print wikipedia.summary(ques)
+    wikipedia.set_lang("en") #change "en" to convenient language for example wikipedia.set_lang("es") will set the language to spanish
+    print wikipedia.summary(ques, sentences=3)
