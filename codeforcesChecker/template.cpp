765ec066965307a626edec375c4b074015c3478a
Jinesh Parakh <50925060+jineshparakh@users.noreply.github.com>
2020-10-03 10:20:24 +0530

    Added Codeforces Checker Python Script

---
+++ codeforcesChecker/template.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define mod 1000000007
+#define N 100005
+#define in insert
+#define pb push_back
+#define mp make_pair
+#define F first
+#define pf push_front
+#define speed ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define test ll t; cin>>t; while(t--)
+ll exp(ll x,ll y,ll p){ ll res=1; while(y) {if(y%2) res=(res*x%p)%p; x=(x*x)%p; y/=2; } return res;}
+ll exp(ll x,ll y){ ll res=1; while(y) {if(y%2) res=(res*x); x=(x*x); y/=2; } return res;}
+
+int main(){
+ 
+    return 0;
+}
+
