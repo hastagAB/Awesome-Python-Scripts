b652b9f6024e82c89468d5f35ffc86a3c36dbdc6
Niranjan B <niranjannb7777@gmail.com>
2023-09-29 15:45:10 +0530

    fix : #259 ANTIALIAS changed to LANCZOS

--- Image_Compressor/Image_Compressor.py
+++ Image_Compressor/Image_Compressor.py
@@ -6,7 +6,7 @@ file_path=askopenfilenames()
 img = PIL.Image.open(file_path)
 myHeight,myWidth = img.size
 
-img=img.resize((myHeight,myWidth),PIL.Image.ANTILIAS)
+img=img.resize((myHeight,myWidth),Image.Resampling.LANCZOS)
 save_path=asksaveasfile()
 
 img.save(save_path+"_compressed.JPG")
